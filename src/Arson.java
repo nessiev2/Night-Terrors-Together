649c94c13427582cfa117f7d6529c64018af3e0d
gyyoi <s300030541@ddsbstudent.ca>
2023-05-28 14:11:31 -0400

    arson was updated

--- src/Arson.java
+++ src/Arson.java
@@ -1,11 +1,33 @@
-public class Arson extends Task {
-    public Arson(boolean isTask) {
+public class Arson extends Task{
+
+    public final int RADIUS = 50;
+    public Arson(boolean isTask){
         super(isTask);
     }
 
-    public void main(String[] args) {
+    public int doTask(boolean isTask, TrashCan[] trash, Player1 p1, Player2 p2) {
+        int counter = 0;
         if (isTask) {
+            while(!isComplete){
+                for (TrashCan t: trash){
+                    double dist1 = Math.sqrt(Math.pow(t.getX()-p1.getX(), 2) + Math.pow(t.getY()-p1.getY(), 2));
+                    double dist2 = Math.sqrt(Math.pow(t.getX()-p2.getX(), 2) + Math.pow(t.getY()-p2.getY(), 2));
 
+                    if (dist1 <= RADIUS && p1.interact){
+                        counter++;
+                    }
+                    if (dist2 <= RADIUS && p2.interact){
+                        counter++;
+                    }
+                }
+                if (counter >= 3){
+                    isComplete = true;
+                    System.out.println("mmm finish");
+                    return 1;
+                }
+            }
         }
+        return 0;
     }
+
 }
