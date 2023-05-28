f05cea0b23300a5c806de434f7c93e83b31e16a9
gyyoi <s300030541@ddsbstudent.ca>
2023-05-28 14:27:46 -0400

    arso n is not as bnuggy

--- src/Arson.java
+++ src/Arson.java
@@ -1,13 +1,12 @@
 public class Arson extends Task{
 
-    public final int RADIUS = 50;
+    public final int RADIUS = 200;
+    public int counter = 0;
     public Arson(boolean isTask){
         super(isTask);
     }
 
     public int doTask(TrashCan[] trash, Player p1, Player p2) {
-        int counter = 0;
-        //while(!isComplete){
             for (TrashCan t: trash){
                 double dist1 = Math.sqrt(Math.pow(t.getX()-p1.getX(), 2) + Math.pow(t.getY()-p1.getY(), 2));
                 double dist2 = Math.sqrt(Math.pow(t.getX()-p2.getX(), 2) + Math.pow(t.getY()-p2.getY(), 2));
@@ -18,13 +17,14 @@ public class Arson extends Task{
                 if (dist2 <= RADIUS && p2.interact){
                     counter++;
                 }
+
+                System.out.println(counter);
             }
             if (counter >= 3){
                 isComplete = true;
                 System.out.println("mmm finish");
                 return 1;
             }
-        //}
 
         return 0;
     }
