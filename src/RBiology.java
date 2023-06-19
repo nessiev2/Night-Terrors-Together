e04c9aed0d8657c16724805c66ccf36d16367e18
gyyoi <s300030541@ddsbstudent.ca>
2023-06-18 20:55:46 -0400

    bio trash poo

--- src/RBiology.java
+++ src/RBiology.java
@@ -41,7 +41,7 @@ public class RBiology {
         this.b1 = b1;
 
         trashCans[0] = new ATrashCan(r.nextInt(1001) + 400,r.nextInt(100) + 290); // top wall
-        trashCans[1] = new ATrashCan(r.nextInt(271),r.nextInt(150) + 270); // left wall
+        trashCans[1] = new ATrashCan(r.nextInt(271),r.nextInt(130) + 270); // left wall
         trashCans[2] = new ATrashCan(r.nextInt(221) + 1450,r.nextInt(451) + 300); //right wall
 
         arsont1 = new DoArson(b1, trashCans[0]);
